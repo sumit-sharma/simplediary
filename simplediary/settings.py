69c31343bab5b180a31825399d57846434df09d7
sumit <211.sumit@gmail.com>
2021-09-05 12:58:09 +0530

    hosting fix

--- simplediary/settings.py
+++ simplediary/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASS'),
-        'HOST': '127.0.0.1',
+        'HOST': env('DATABASE_HOST'),
         'PORT': '3306',
     }
 }
