d2d8b1ed2aa3cf0b9cde3d8e8b45914f37ea6a6d
sumit <211.sumit@gmail.com>
2021-09-12 22:02:59 +0530

    WIP: login

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,15 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.contrib.auth import views
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
-    path('petroleum/', include('petroleum.urls')),
-    # login and registration
-    path('login', views.LoginView.as_view(), name='login'),
+    path('', views.index, name='index'),
 ]
--- simplediary/settings.py
+++ simplediary/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -147,3 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK =  'bootstrap4'
+
+LOGIN_REDIRECT_URL =  '/'
